0e97fdc277445fce302d40fab68de4d7fc37c89b
KatarinaBelojica <33135003+KatarinaBelojica@users.noreply.github.com>
2020-08-18 09:15:11 +0200

    update

--- website/js/date.js
+++
@@ -1,6 +0,0 @@
-function showDate() {
-var d = new Date(),
-    months = ['Januar','Februar','Mart','April','Maj','Jun','Jul','Avgust','Septembar','Octobar','Novembar','Decembar'],
-    days = ['Nedelja','Ponedeljak','Utorak','Sreda','Četvrtak','Petak','Subota'];
-return days[d.getDay()]+' , '+d.getDate()+'. '+months[d.getMonth()]+' '+d.getFullYear()+'.';
-}
